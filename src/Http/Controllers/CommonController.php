a2a20d55ebb507e44e68f0aac4f3a9634b88ca36
eiixy <990656271@qq.com>
2020-04-15 17:17:22 +0800

    修改接口描述

--- src/Http/Controllers/CommonController.php
+++ src/Http/Controllers/CommonController.php
@@ -17,14 +17,14 @@ class CommonController extends Controller
         $routes = app()->routes->getRoutes();
         $list = [];
         foreach ($routes as $route) {
-            if (key_exists('middleware',$route->action)) {
+            if (key_exists('middleware', $route->action)) {
                 $middleware = $route->action['middleware'];
                 foreach (Arr::wrap($middleware) as $item) {
                     if (Str::startsWith($item, 'rbac.check:')) {
                         $list[] = [
                             'uri' => $route->uri,
                             'methods' => $route->methods,
-                            'label' => explode(':', $item)[1],
+                            'label' => $route->action['description'] ?? explode(':', $item)[1],
                             'value' => explode(':', $item)[1]
                         ];
                     }
